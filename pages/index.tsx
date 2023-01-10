d2c3bcee90166c3afbf5e1b7d4488b86cb58cae3
David Shin <david.shin@mirpartners.com>
2023-01-10 12:31:58 -0800

    add login/logout functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,87 @@
-import Head from "next/head"
-import Image from "next/image"
-import { Inter } from "@next/font/google"
-import styles from "../styles/Home.module.css"
-
-const inter = Inter({ subsets: ["latin"] })
+import firebase from "../firebase/clientApp"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { useCollection } from "react-firebase-hooks/firestore"
+import { Box, Button, Center, Flex, Link, Text } from "@chakra-ui/react"
+import "firebase/compat/auth"
 
 export default function Home() {
-	return <div>Hello</div>
+	const db = firebase.firestore()
+	const [user, loading, error] = useAuthState(firebase.auth())
+	const [votes, votesLoading, votesError] = useCollection(
+		firebase.firestore().collection("votes")
+	)
+
+	if (!votesLoading && votes) {
+		votes.docs.map((doc) => console.log(doc.data()))
+	}
+
+	//Create document function
+	const addVoteDocument = async (vote: string) => {
+		if (!user) return
+		await db.collection("votes").doc(user.uid).set({
+			vote,
+		})
+	}
+	return (
+		<Box h="100vh">
+			<Center h="100%">
+				<Flex flexDirection="column">
+					<Text fontSize="5xl" fontWeight="bold" mb="8">
+						Pineapple on Pizza?
+					</Text>
+					<Center>
+						<Flex flexDir="column">
+							<Box mb="5">
+								<Button
+									fontSize="5xl"
+									w="100%"
+									h="110px"
+									onClick={() => addVoteDocument("yes")}
+									border="2px solid lightgray"
+								>
+									✅🍍🍕
+								</Button>
+								<Text fontSize="2xl" fontWeight="bold">
+									Pineapple Lovers:{" "}
+									{
+										votes?.docs?.filter((doc) => doc.data().vote === "yes")
+											.length
+									}
+								</Text>
+							</Box>
+							<Box>
+								<Button
+									fontSize="5xl"
+									w="100%"
+									h="110px"
+									onClick={() => addVoteDocument("no")}
+									border="2px solid lightgray"
+								>
+									❌🍍🍕
+								</Button>
+								<Text fontSize="2xl" fontWeight="bold">
+									Pineapple Haters:{" "}
+									{
+										votes?.docs?.filter((doc) => doc.data().vote === "no")
+											.length
+									}
+								</Text>
+							</Box>
+							<Center>
+								<Box mt="10">
+									{user ? (
+										<Link onClick={() => firebase.auth().signOut()}>
+											Logout
+										</Link>
+									) : (
+										<Link href={"/auth"}>Login to vote!</Link>
+									)}
+								</Box>
+							</Center>
+						</Flex>
+					</Center>
+				</Flex>
+			</Center>
+		</Box>
+	)
 }
